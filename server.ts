8661fdfe1c58478c49dd1eea9477568f59134f5e
LeadDev <leaddev@anon.farm>
2022-08-04 16:12:56 +0530

    Update scripts

---
+++ server.ts
@@ -0,0 +1,63 @@
+import { app } from "./app";
+import Debugger from "debug";
+import http from "http";
+
+const debug = Debugger("apis:server");
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+function normalizePort(val: string) {
+  const port = parseInt(val, 10);
+
+  // named pipe
+  if (isNaN(port)) return val;
+
+  // port number
+  if (port >= 0) return port;
+
+  return false;
+}
+
+function onError(error: any) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  const addr = server.address();
+  const bind =
+    addr !== null
+      ? typeof addr === "string"
+        ? "pipe " + addr
+        : "port " + addr.port
+      : "address is null";
+  debug("Listening on " + bind);
+}
