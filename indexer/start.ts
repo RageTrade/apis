8ddac15e2f2306ddde19812c10b203e3c7511d0d
LeadDev <leaddev@anon.farm>
2022-09-27 18:08:22 +0000

    Start indexer for arbgoerli

--- indexer/start.ts
+++ indexer/start.ts
@@ -2,3 +2,4 @@ import { AccountCreatedIndexer } from "./account-created";
 
 new AccountCreatedIndexer("arbmain", 17185390, 5000).start();
 new AccountCreatedIndexer("arbtest", 12705265, 5000).start();
+new AccountCreatedIndexer("arbgoerli", 408336, 5000).start();
