a1421ef3b5fa074ccf375fb53389397fe0e285a8
LeadDev <leaddev@anon.farm>
2022-10-14 11:50:01 +0000

    Discontinue arbrinkeby indexer

--- indexer/start.ts
+++ indexer/start.ts
@@ -2,5 +2,5 @@ import { AccountCreatedIndexer } from "./account-created";
 
 new AccountCreatedIndexer("arbmain", 17185390, 5000).start();
 // new AccountCreatedIndexer("arbtest", 12705265, 5000).start();
-new AccountCreatedIndexer("arbrinkeby", 12705265, 5000).start();
+// new AccountCreatedIndexer("arbrinkeby", 12705265, 5000).start();
 new AccountCreatedIndexer("arbgoerli", 408336, 5000).start();
