3d5fd18d7d53459c5d5bc7ebe3dc9eda8820c033
LeadDev <leaddev@anon.farm>
2022-11-18 08:35:55 +0000

    Update sdk

--- scripts/v2/get-vault-info-fast.ts
+++ scripts/v2/get-vault-info-fast.ts
@@ -1,16 +1,21 @@
-import { ethers } from "ethers";
+import { BigNumber, ethers } from "ethers";
 
 import {
   Amount,
   bigNumberToAmount,
+  DnGmxJuniorVault__factory,
   getVault,
   IERC20Metadata__factory,
   NetworkName,
+  priceX128ToPrice,
+  Q128,
   stringifyBigNumber,
+  stringToAmount,
   VaultName,
 } from "@ragetrade/sdk";
 
 import { getProvider } from "../../providers";
+import { parseUnits } from "ethers/lib/utils";
 
 export async function getVaultInfoFast(
   networkName: NetworkName,
@@ -26,6 +31,9 @@ export interface VaultInfoFastResult {
   totalShares: Amount;
   totalAssets: Amount;
   vaultMarketValue: Amount;
+  assetsPerShare: Amount;
+  assetPrice: Amount;
+  sharePrice: Amount;
 }
 const USD_DECIMALS = 6;
 
@@ -51,6 +59,44 @@ export async function getVaultInfoFastSDK(
     assetDecimals
   );
 
+  // asset price
+  let assetPrice: Amount;
+  let assetPriceX128: BigNumber;
+  try {
+    assetPriceX128 = await vault.getPriceX128(); // dollars per asset
+    assetPrice = stringToAmount(
+      (
+        await priceX128ToPrice(assetPriceX128, USD_DECIMALS, assetDecimals)
+      ).toFixed(USD_DECIMALS),
+      USD_DECIMALS
+    );
+  } catch {
+    const priceD18 = await DnGmxJuniorVault__factory.connect(
+      vault.address,
+      provider
+    ).getPrice(false);
+    assetPrice = bigNumberToAmount(priceD18, 18);
+    assetPriceX128 = priceD18.mul(Q128).div(BigNumber.from(10).pow(18 + 12));
+  }
+
+  // share price
+  const assetsPerShareDX = await vault.convertToAssets(
+    parseUnits("1", shareDecimals)
+  );
+  const assetsPerShare = bigNumberToAmount(assetsPerShareDX, assetDecimals);
+  const sharePrice = stringToAmount(
+    (
+      await priceX128ToPrice(
+        assetPriceX128
+          .mul(assetsPerShareDX)
+          .div(parseUnits("1", assetDecimals)),
+        6,
+        shareDecimals
+      )
+    ).toFixed(USD_DECIMALS),
+    USD_DECIMALS
+  );
+
   // vault market value
   const vaultMarketValueUSD = await vault.getVaultMarketValue();
   const vaultMarketValue = bigNumberToAmount(vaultMarketValueUSD, USD_DECIMALS);
@@ -60,5 +106,8 @@ export async function getVaultInfoFastSDK(
     totalShares: totalSupply,
     totalAssets,
     vaultMarketValue,
+    assetsPerShare,
+    assetPrice,
+    sharePrice,
   };
 }
