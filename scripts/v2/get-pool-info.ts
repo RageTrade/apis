64e330c1c1e242f202bacdf86a52ab2405cdd283
LeadDev <leaddev@anon.farm>
2022-11-23 15:30:46 +0000

    Add error for incorrect pools

--- scripts/v2/get-pool-info.ts
+++ scripts/v2/get-pool-info.ts
@@ -1,17 +1,41 @@
-import { BigNumberish } from "ethers";
+import { BigNumber, BigNumberish } from "ethers";
 
 import {
   getPoolInfo as getPoolInfoSDK,
   NetworkName,
   stringifyBigNumber,
+  pools,
 } from "@ragetrade/sdk";
 
 import { getProvider } from "../../providers";
+import { ErrorWithStatusCode } from "../../utils";
 
 export async function getPoolInfo(
   networkName: NetworkName,
   poolId: BigNumberish
 ) {
+  if (pools[networkName] === undefined) {
+    throw new ErrorWithStatusCode(
+      `NetworkName ${networkName} is not valid or does not contain pools in the sdk.`,
+      400
+    );
+  }
+
+  let poolFound = false;
+  for (const pool of pools[networkName]) {
+    if (BigNumber.from(pool.poolId).eq(poolId)) {
+      poolFound = true;
+      break;
+    }
+  }
+
+  if (!poolFound) {
+    throw new ErrorWithStatusCode(
+      `PoolId ${poolId} is incorrect for this network or sdk is not updated with latest pool.`,
+      400
+    );
+  }
+
   const provider = getProvider(networkName);
   const result = await getPoolInfoSDK(provider, poolId);
   return stringifyBigNumber(result);
