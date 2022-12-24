3972f05c3befd218c73c24cd622c0e81f533ffc9
LeadDev <leaddev@anon.farm>
2022-12-24 16:03:50 +0000

    Update reward hardcodings

--- scripts/v2/dn-gmx-vault-apy/index.ts
+++ scripts/v2/dn-gmx-vault-apy/index.ts
@@ -18,7 +18,7 @@ export const getDnGmxApyBreakdown = async (networkName: NetworkName) => {
 
   const seniorVault = {
     aaveSupplyApy: supplyApy,
-    glpRewardsPct: 4.4, // ethRewards[1],
+    glpRewardsPct: 4.55, // ethRewards[1],
     _glpRewardsPct: ethRewards[1],
   };
 
@@ -26,7 +26,7 @@ export const getDnGmxApyBreakdown = async (networkName: NetworkName) => {
     btcBorrowApy: borrowApy[0],
     ethBorrowApy: borrowApy[1],
     glpTraderPnl: traderPnl,
-    glpRewardsPct: 13.68, // ethRewards[0],
+    glpRewardsPct: 14.25, // ethRewards[0],
     _glpRewardsPct: ethRewards[0],
     esGmxRewards,
   };
