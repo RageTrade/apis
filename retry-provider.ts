2523015c5a98f596f3c296d0ba4075ef373a10b5
LeadDev <leaddev@anon.farm>
2023-01-12 10:25:35 +0000

    Add error logs in retry provider

--- retry-provider.ts
+++ retry-provider.ts
@@ -7,12 +7,16 @@ export class RetryProvider extends ethers.providers.StaticJsonRpcProvider {
   async send(method: string, params: Array<any>): Promise<any> {
     for (let i = 0; i < 5; i++) {
       try {
-        return await super.send(method, params);
+        const result = await super.send(method, params);
+        if (i > 0) {
+          console.error(`RetryProvider: Success after ${i} retries: ${method}`);
+        }
+        return result;
       } catch (e: any) {
         if (e?.serverError?.code === "ECONNRESET") {
           // for ECONNRESET, wait for 0 to 2 sec and retry
           console.error(
-            `ECONNRESET at time ${Date.now()}, retrying ${i}: ${method}`
+            `RetryProvider: ECONNRESET at time ${Date.now()}, retrying ${i}: ${method}`
           );
           await new Promise((res) =>
             setTimeout(res, Math.floor(Math.random() * 2000))
