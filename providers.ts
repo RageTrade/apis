af7154a2c391d6f2e4ebaf08b5205aba3f8f95d9
LeadDev <leaddev@anon.farm>
2023-01-12 07:32:41 +0000

    Change mainnet rpc to ankr

--- providers.ts
+++ providers.ts
@@ -7,7 +7,8 @@ config();
 
 export const arbmain = new ethers.providers.StaticJsonRpcProvider(
   // "https://arb1.arbitrum.io/rpc"
-  "https://arb-mainnet.g.alchemy.com/v2/" + process.env.ALCHEMY_KEY
+  // "https://arb-mainnet.g.alchemy.com/v2/" + process.env.ALCHEMY_KEY,
+  "https://rpc.ankr.com/arbitrum"
 );
 export const arbtest = new ethers.providers.StaticJsonRpcProvider(
   // "https://rinkeby.arbitrum.io/rpc"
