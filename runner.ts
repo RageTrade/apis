34173e2464c09f3b5eb446bbdd8079cd8ff7e409
LeadDev <leaddev@anon.farm>
2023-01-16 10:33:33 +0000

    Add runner.ts to run scripts

---
+++ runner.ts
@@ -0,0 +1,7 @@
+import path from "path";
+
+const req = require(path.resolve(process.cwd(), process.argv[2]));
+
+(Object.values(req)[0] as Function)(...process.argv.slice(3)).then((res: any) =>
+  console.log(JSON.stringify(res))
+);
