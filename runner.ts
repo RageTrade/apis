9f52e02ad0abfe9fed9780fa98b64e0809c94f86
LeadDev <leaddev@anon.farm>
2023-01-26 14:51:34 +0000

    Print script output

--- runner.ts
+++ runner.ts
@@ -6,8 +6,14 @@ const req = require(path.resolve(process.cwd(), process.argv[2]));
 
 (Object.values(req)[0] as Function)(...process.argv.slice(3)).then(
   (res: any) => {
-    if (process.argv[process.argv.length - 1] === "print") {
-      console.log(JSON.stringify(res));
+    const str = JSON.stringify(res, null, 2);
+    if (
+      str.length < 1000 ||
+      process.argv[process.argv.length - 1] === "print"
+    ) {
+      console.log(str);
+    } else {
+      console.log("Result too long to print, just add ' print' to the end");
     }
     process.exit();
   }
