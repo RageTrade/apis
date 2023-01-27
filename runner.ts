f21da64aa816e51579fd9ecca3f8b8da2c97723f
LeadDev <leaddev@anon.farm>
2023-01-27 12:35:23 +0000

    Update runner script

--- runner.ts
+++ runner.ts
@@ -7,13 +7,15 @@ const req = require(path.resolve(process.cwd(), process.argv[2]));
 (Object.values(req)[0] as Function)(...process.argv.slice(3)).then(
   (res: any) => {
     const str = JSON.stringify(res, null, 2);
-    if (
-      str.length < 1000 ||
-      process.argv[process.argv.length - 1] === "print"
-    ) {
-      console.log(str);
-    } else {
-      console.log("Result too long to print, just add ' print' to the end");
+    if (str) {
+      if (
+        str.length < 1000 ||
+        process.argv[process.argv.length - 1] === "print"
+      ) {
+        console.log(str);
+      } else {
+        console.log("Result too long to print, just add ' print' to the end");
+      }
     }
     process.exit();
   }
