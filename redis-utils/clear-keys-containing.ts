3168fe9fa728b3cd4f898872ae2b1792c442fa5a
LeadDev <leaddev@anon.farm>
2023-02-03 17:29:02 +0000

    Add more redis utils

--- redis-utils/clear-keys-containing.ts
+++ redis-utils/clear-keys-containing.ts
@@ -1,16 +1,26 @@
-import { getRedisClient } from "./get-client";
+import { getRedisClient } from './get-client'
 
-export async function clearKeysContaining(match: (key: string) => boolean) {
-  const client = getRedisClient();
-  let cursor: string | number = 0;
-  let keys = [];
-  while (cursor !== "0") {
-    [cursor, keys] = await client.scan(cursor);
+export async function clearKeysContaining(
+  match: (key: string) => boolean,
+  _onlyTemporary: boolean | 'true' | 'false' = true
+) {
+  const onlyTemporary = _onlyTemporary === true || _onlyTemporary === 'true'
+  const client = getRedisClient()
+  let cursor: string | number = 0
+  let keys = []
+  let count = 0
+  let deleted = 0
+  while (cursor !== '0') {
+    ;[cursor, keys] = await client.scan(cursor)
     for (const key of keys) {
-      const ttl = await client.ttl(key);
+      const ttl = onlyTemporary ? await client.ttl(key) : -2
       if (ttl !== -1 && match(key)) {
-        await client.del(key);
+        await client.del(key)
+        deleted++
       }
+      count += keys.length
     }
   }
+  console.log('iteration completed on total keys:', count)
+  console.log('deleted keys:', deleted)
 }
