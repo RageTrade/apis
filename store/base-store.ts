68204a4c24e2353dea3085c85c4dfd3ccedba7ef
fr0ntenddev <fr0ntenddev@rage.trade>
2023-02-03 16:25:04 +0530

    fix: modify types for base store

--- store/base-store.ts
+++ store/base-store.ts
@@ -7,7 +7,7 @@ export interface Internal {
 }
 
 export class BaseStore<Value> {
-  _promises = new Map<string, any>()
+  _promises = new Map<string, Promise<Value>>()
   _timestampPrepend = '__timestamp_'
 
   /**
@@ -16,33 +16,40 @@ export class BaseStore<Value> {
    * @param _valueFn Function that should return the value for setting if it doesn't exist.
    * @returns The value.
    */
-  async getOrSet<V = Value>(
+  async getOrSet(
     key: string,
-    valueFn: () => V | Promise<V>,
+    valueFn: () => Value | Promise<Value>,
     secondsOld?: number
-  ): Promise<V> {
-    const read = await this.get<V>(key, secondsOld)
+  ): Promise<Value> {
+    const read = await this.get(key, secondsOld)
 
-    let valuePromise = this._promises.get(key)
+    const valuePromise = this._promises.get(key)
+
     if (read !== undefined) {
       debug('getOrSet: returning the value present in storage')
+
       return read
     } else if (valuePromise) {
       debug('getOrSet: value being queried already, waiting for it')
+
       return valuePromise
     } else {
       debug('getOrSet: value not present in storage, fetching it')
-      valuePromise = valueFn()
-      if (valuePromise instanceof Promise) {
-        this._promises.set(key, valuePromise)
+
+      const newValuePromise = valueFn()
+
+      if (newValuePromise instanceof Promise) {
+        this._promises.set(key, newValuePromise)
       }
+
       try {
-        const value = await valuePromise
-        await this.set<V>(key, value)
-        this._promises.set(key, undefined)
+        const value = await newValuePromise
+        await this.set(key, value)
+        this._promises.delete(key)
+
         return value
       } catch (e) {
-        this._promises.set(key, undefined)
+        this._promises.delete(key)
         throw e
       }
     }
@@ -50,25 +57,25 @@ export class BaseStore<Value> {
 
   /**
    * Gets the value from storage, or undefined if it doesn't exist.
-   * @param _key The key to get.
+   * @param key The key to get.
    * @returns The value, or undefined if it doesn't exist.
    */
-  async get<V = Value>(_key: string, secondsOld?: number): Promise<V | undefined> {
-    const value = this._get<V>(_key)
+  async get(key: string, secondsOld?: number): Promise<Value | undefined> {
+    const value = this._get<Value>(key)
     // if value not present just return undefined immediately
     if (value === undefined) return undefined
     if (value !== undefined && secondsOld !== undefined) {
-      const creationTimestamp = await this._get<number>(this._timestampPrepend + _key)
+      const creationTimestamp = await this._get<number>(this._timestampPrepend + key)
       if (creationTimestamp === undefined && !!this._timestampPrepend) {
         // if there is no timestamp, set current timestamp
-        await this._set(this._timestampPrepend + _key, currentTimestamp())
+        await this._set(this._timestampPrepend + key, currentTimestamp())
       } else if (
         creationTimestamp !== undefined &&
         creationTimestamp !== -1 &&
         creationTimestamp + secondsOld < currentTimestamp()
       ) {
         // if the timestamp is older than the secondsOld, delete the value
-        await this._set(_key, undefined)
+        await this._set(key, undefined)
         return undefined
       }
     }
@@ -77,16 +84,16 @@ export class BaseStore<Value> {
 
   /**
    * Sets the value in storage.
-   * @param _key The key to set.
-   * @param _value The value to set.
+   * @param key The key to set.
+   * @param value The value to set.
    * @returns Promise that resolves when the value is set.
    */
-  async set<V = Value>(_key: string, _value: V): Promise<void> {
+  async set(key: string, value: Value): Promise<void> {
     // set value
-    await this._set(_key, _value)
+    await this._set(key, value)
     // set timestamp
     if (this._timestampPrepend) {
-      await this._set(this._timestampPrepend + _key, currentTimestamp())
+      await this._set(this._timestampPrepend + key, currentTimestamp())
     }
   }
 
