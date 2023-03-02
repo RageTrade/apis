0a68b3186e776ba775d41fc1d1644d53ba56b2e4
LeadDev <leaddev@anon.farm>
2023-03-02 10:02:39 +0000

    Add a print prompt on runner script

--- runner.ts
+++ runner.ts
@@ -1,16 +1,27 @@
 import { config } from 'dotenv'
 import path from 'path'
+import yesno from 'yesno'
+
 config()
 
 const req = require(path.resolve(process.cwd(), process.argv[2]))
 
-;(Object.values(req)[0] as Function)(...process.argv.slice(3)).then((res: any) => {
+;(Object.values(req)[0] as Function)(...process.argv.slice(3)).then(async (res: any) => {
   const str = JSON.stringify(res, null, 2)
   if (str) {
     if (str.length < 1000 || process.argv[process.argv.length - 1] === 'print') {
       console.log(str)
     } else {
-      console.log("Result too long to print, just add ' print' to the end")
+      console.log(
+        "Result too long to print, just add ' print' to the end or respond to the question next:"
+      )
+
+      const answer = await yesno({
+        question: 'Print result?'
+      })
+      if (answer) {
+        console.log(str)
+      }
     }
   }
   process.exit()
