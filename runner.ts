703ea261d687156b31c496dc262886b1950780d5
LeadDev <leaddev@anon.farm>
2023-03-09 10:38:46 +0000

    Parse booleans in runner

--- runner.ts
+++ runner.ts
@@ -7,34 +7,40 @@ config()
 
 const req = require(path.resolve(process.cwd(), process.argv[2]))
 
-;(Object.values(req)[0] as Function)(...process.argv.slice(3)).then(async (res: any) => {
-  const str = JSON.stringify(res, null, 2)
-  if (str) {
-    if (str.length < 1000 || process.argv[process.argv.length - 1] === 'print') {
-      console.log(str)
-    } else {
-      console.log(
-        "Result too long to print, just add ' print' to the end or respond to the question next:"
-      )
-
-      const getFileName = (i: number) => `out${i || ''}.json`
-      let i = 0
-      let fileName = ''
-      while (fs.existsSync((fileName = getFileName(i)))) {
-        i++
-      }
-
-      const answer = await yesno({
-        question: `Press "y" to print in console or "n" to write the output to "${fileName}" file`
-      })
-      if (answer) {
+;(Object.values(req)[0] as Function)(...parseBooleans(process.argv.slice(3))).then(
+  async (res: any) => {
+    const str = JSON.stringify(res, null, 2)
+    if (str) {
+      if (str.length < 1000 || process.argv[process.argv.length - 1] === 'print') {
         console.log(str)
       } else {
-        console.log(`writing to ${fileName} please wait...`)
-        fs.writeFileSync(fileName, str)
-        console.log(`done`)
+        console.log(
+          "Result too long to print, just add ' print' to the end or respond to the question next:"
+        )
+
+        const getFileName = (i: number) => `out${i || ''}.json`
+        let i = 0
+        let fileName = ''
+        while (fs.existsSync((fileName = getFileName(i)))) {
+          i++
+        }
+
+        const answer = await yesno({
+          question: `Press "y" to print in console or "n" to write the output to "${fileName}" file`
+        })
+        if (answer) {
+          console.log(str)
+        } else {
+          console.log(`writing to ${fileName} please wait...`)
+          fs.writeFileSync(fileName, str)
+          console.log(`done`)
+        }
       }
     }
+    process.exit()
   }
-  process.exit()
-})
+)
+
+function parseBooleans(argv: string[]) {
+  return argv.map((arg) => (arg === 'true' ? true : arg === 'false' ? false : arg))
+}
