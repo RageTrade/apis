0045d1ac0e32a608cd5c66a121ba5305cd4b38ec
LeadDev <leaddev@anon.farm>
2023-03-09 16:36:03 +0000

    Update block numbers

--- scripts/aggregated/glp-pnl.ts
+++ scripts/aggregated/glp-pnl.ts
@@ -52,8 +52,8 @@ export async function getGlpPnl(
   const { dnGmxJuniorVault, dnGmxBatchingManager } =
     deltaNeutralGmxVaults.getContractsSync(networkName, provider)
 
-  const startBlock = 67762250
-  const endBlock = 67829400
+  const startBlock = 65567250
+  const endBlock = 68048150
   // const startBlock = 65567250
   // const endBlock = await provider.getBlockNumber()
   const interval = 500
