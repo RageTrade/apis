116af598dc19c29f66339f0fe93f539e870da9c6
LeadDev <leaddev@anon.farm>
2023-03-13 11:40:15 +0000

    Fix error handler issue

--- app.ts
+++ app.ts
@@ -32,8 +32,7 @@ app.use('/', router)
 app.use(function (_req, _res, next) {
   next(createError(404))
 })
-
-const errorHandler: ErrorRequestHandler = function (err, req, res) {
+const errorHandler: ErrorRequestHandler = function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
@@ -42,5 +41,6 @@ const errorHandler: ErrorRequestHandler = function (err, req, res) {
   const status = err.status || 500
   res.status(status)
   res.json({ error: err.message, status })
+  next()
 }
 app.use(errorHandler)
