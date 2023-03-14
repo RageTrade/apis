682a4c295f671fd6ad7f99b3a1a025d4d0fb3f5f
LeadDev <leaddev@anon.farm>
2023-03-14 10:28:04 +0000

    Add pagination script

--- json-to-csv.ts
+++ json-to-csv.ts
@@ -5,8 +5,22 @@ const converter = require('json-2-csv')
 export async function apiCsv(url: string) {
   console.log('apiCsv called')
 
-  const json = await fetchJson({ url, timeout: 10000000 })
-  const str = await json2csv(json)
+  let array: any[] = []
+  let pageNumber = 1
+  while (true) {
+    console.log('pageNumber', pageNumber)
+    const resp = await fetchJson({
+      url: url + '&pageSize=5000&pageNumber=' + pageNumber,
+      timeout: 10000000
+    })
+    array = [...array, ...resp.result.data]
+    if (resp.result.data.length === 5000) {
+      pageNumber++
+    } else {
+      break
+    }
+  }
+  const str = await json2csv(array)
 
   const getFileName = (i: number) => `out${i || ''}.csv`
   let i = 0
