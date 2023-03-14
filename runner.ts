3c0168deb58a935ec9eb84263428c8d3a3eaab3c
LeadDev <leaddev@anon.farm>
2023-03-14 10:53:19 +0000

    Update runner script

--- runner.ts
+++ runner.ts
@@ -1,8 +1,8 @@
 import { config } from 'dotenv'
 import path from 'path'
-import yesno from 'yesno'
 import fs from 'fs-extra'
 import { json2csv } from './json-to-csv'
+const promptly = require('promptly')
 
 config()
 
@@ -11,35 +11,45 @@ const req = require(path.resolve(process.cwd(), process.argv[2]))
 const args = parseBooleans(process.argv.slice(3))
 console.log(`Running ${Object.keys(req)[0]}(${args.map((str) => `"${str}"`).join(',')})`)
 ;(Object.values(req)[0] as Function)(...args).then(async (res: any) => {
-  // const str = JSON.stringify(res, null, 2)
-  const str = await json2csv(res)
-  if (str) {
-    if (str.length < 10 || process.argv[process.argv.length - 1] === 'print') {
+  while (true) {
+    const question = 'Press P for print on console, C for CSV file and J for JSON file'
+    let answer = await promptly.prompt(question)
+    answer = answer.toLowerCase()
+    if (answer === 'p' || answer === 'print') {
+      // PRINT
+      const str = JSON.stringify(res, null, 2)
       console.log(str)
-    } else {
-      console.log(
-        "Result too long to print, just add ' print' to the end or respond to the question next:"
-      )
-
+    } else if (answer === 'c' || answer === 'csv') {
+      // CSV
+      const str = await json2csv(res)
       const getFileName = (i: number) => `out${i || ''}.csv`
       let i = 0
       let fileName = ''
       while (fs.existsSync((fileName = getFileName(i)))) {
         i++
       }
-
-      const answer = await yesno({
-        question: `Press "y" to print in console or "n" to write the output to "${fileName}" file`
-      })
-      if (answer) {
-        console.log(str)
-      } else {
-        console.log(`writing to ${fileName} please wait...`)
-        fs.writeFileSync(fileName, str)
-        console.log(`done`)
+      console.log(`writing to ${fileName} please wait...`)
+      fs.writeFileSync(fileName, str)
+      console.log(`done`)
+    } else if (answer === 'j' || answer === 'json') {
+      // JSON
+      const str = JSON.stringify(res, null, 2)
+      const getFileName = (i: number) => `out${i || ''}.json`
+      let i = 0
+      let fileName = ''
+      while (fs.existsSync((fileName = getFileName(i)))) {
+        i++
       }
+      console.log(`writing to ${fileName} please wait...`)
+      fs.writeFileSync(fileName, str)
+      console.log(`done`)
+    } else {
+      console.log('answer not recognized. ')
+      continue
     }
+    break
   }
+
   process.exit()
 })
 
