93f85efeed3bd5b49616db0573ad794cc2d92a98
LeadDev <leaddev@anon.farm>
2023-03-15 09:02:52 +0000

    Update util

--- redis-utils/clear-aggregated-api-cache.ts
+++ redis-utils/clear-aggregated-api-cache.ts
@@ -1,21 +1,5 @@
-import { getRedisClient } from './get-client'
+import { clearKeysContaining } from './clear-keys-containing'
 
-export async function clearAggregatedApiCache() {
-  const client = getRedisClient()
-  let cursor: string | number = 0
-  let keys = []
-  let total = 0
-  let total2 = 0
-  while (cursor !== '0') {
-    ;[cursor, keys] = await client.scan(cursor)
-    for (const key of keys) {
-      if (key.includes('aggregated')) {
-        total2 += 1
-        await client.del(key)
-      }
-    }
-    total += keys.length
-  }
-  console.log('iteration completed on total keys:', total)
-  console.log('deleted keys:', total2)
+export async function clearErrorsInArchiveCacheProvider() {
+  await clearKeysContaining((key) => key.includes('aggregated'), false)
 }
