54c475432aa4a5c795fbb11140cc27ee3a05aeb9
LeadDev <leaddev@anon.farm>
2023-03-21 13:02:57 +0000

    Add clearEmptyCallResult util

---
+++ redis-utils/clear-empty-call-result.ts
@@ -0,0 +1,25 @@
+import { getRedisClient } from './get-client'
+
+export async function clearEmptyCallResult() {
+  const client = getRedisClient()
+  let cursor: string | number = 0
+  let keys = []
+  let total = 0
+  let total2 = 0
+  while (cursor !== '0') {
+    ;[cursor, keys] = await client.scan(cursor)
+    for (const key of keys) {
+      if (!key.includes('-call-')) {
+        continue
+      }
+      const result = await client.get(key)
+      if (result === '0x') {
+        total2 += 1
+        await client.del(key)
+      }
+    }
+    total += keys.length
+  }
+  console.log('iteration completed on total keys:', total)
+  console.log('deleted keys:', total2)
+}
