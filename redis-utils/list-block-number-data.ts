0feac9861c80d06ccc5ba5ed7dcef41becba3564
LeadDev <leaddev@anon.farm>
2023-03-28 06:41:28 +0000

    Add return in script

--- redis-utils/list-block-number-data.ts
+++ redis-utils/list-block-number-data.ts
@@ -1,5 +1,5 @@
 import { listKeysContaining } from './list-keys-containing'
 
 export async function listBlockNumberData(blockNumber: number) {
-  await listKeysContaining((key) => key.includes(`-${blockNumber}-`), false)
+  return await listKeysContaining((key) => key.includes(`-${blockNumber}-`), false)
 }
