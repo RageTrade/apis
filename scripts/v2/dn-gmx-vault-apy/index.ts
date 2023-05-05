4a57db3618b421c10f48dca13e39848f65215a25
LeadDev <leaddev@anon.farm>
2023-05-05 06:47:53 +0000

    Remove trader pnl

--- scripts/v2/dn-gmx-vault-apy/index.ts
+++ scripts/v2/dn-gmx-vault-apy/index.ts
@@ -7,8 +7,14 @@ import { getSupplyApy } from './supply-rate'
 import { getTraderPnl } from './trader-pnl'
 
 export const getDnGmxApyBreakdown = async (networkName: NetworkName) => {
-  const [traderPnl, supplyApy, borrowApy, ethRewards, esGmxRewards] = await Promise.all([
-    getTraderPnl(networkName),
+  const [
+    // traderPnl,
+    supplyApy,
+    borrowApy,
+    ethRewards,
+    esGmxRewards
+  ] = await Promise.all([
+    // getTraderPnl(networkName),
     getSupplyApy(networkName),
     getBorrowApy(networkName),
     getEthRewards(networkName),
@@ -23,7 +29,7 @@ export const getDnGmxApyBreakdown = async (networkName: NetworkName) => {
   const juniorVault = {
     btcBorrowApy: borrowApy[0],
     ethBorrowApy: borrowApy[1],
-    glpTraderPnl: traderPnl,
+    // glpTraderPnl: traderPnl,
     glpRewardsPct: ethRewards[0],
     esGmxRewards
   }
